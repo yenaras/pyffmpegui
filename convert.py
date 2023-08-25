b9a64d416fb5588c9ca981e4fc9600b62430d0fb
yenaras <brandon82890@gmail.com>
2023-08-24 23:48:11 -0400

    finished logic but GUI is crashing now

--- convert.py
+++ convert.py
@@ -7,15 +7,22 @@ import ffmpeg
 class Converter(QObject):
     # Define custom signals
     progressed = pyqtSignal(int)
-    converted_files = pyqtSignal(Path)
+    converted_file = pyqtSignal(Path)
     finished = pyqtSignal()
 
-    def __init__(self, files, src_extension, dest_extension):
+    def __init__(self, files, dest_extension):
         super().__init__()
         self.files = files
-        self.src_extension = src_extension
         self.dest_extension = dest_extension
 
     # TODO: convert files with ffmpeg based on suffix and convert option
-    def convertFiles(self):
-        ...
+    def convert_files(self):
+        for file_number, file in enumerate(self.files, 1):
+            new_file = file.parent.joinpath(
+                f"{file.with_suffix(self.dest_extension)}"
+            )
+            ffmpeg.input(str(file)).output(str(new_file)).run()
+            self.progressed.emit(file_number)
+            self.converted_file.emit(new_file)
+        self.progressed.emit(0)
+        self.finished.emit()
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QWidget, QFileDialog
 from collections import deque
 from pathlib import Path
-
+from convert import Converter
 from window import Ui_Window
 
 FILTERS = ";;".join(
@@ -24,10 +25,18 @@ class Window(QWidget, Ui_Window):
 
     def _setupUI(self):
         self.setupUi(self)
+        self.updateStateWhenNoFiles()
+
+    def updateStateWhenNoFiles(self):
+        self.files_count = len(self.files)
+        self.loadFilesButton.setEnabled(True)
+        self.loadFilesButton.setFocus(True)
+        self.convertButton.setEnabled(False)
 
     def _connectSignalsSlots(self):
         # connect load files
         self.loadFilesButton.clicked.connect(self.load_files)
+        self.convertButton.clicked.connect(self.convert_files)
 
     def load_files(self):
         self.dstFileList.clear()
@@ -40,17 +49,54 @@ class Window(QWidget, Ui_Window):
         else:
             init_dir = str(Path.home())
 
-        files, filters = QFileDialog.getOpenFileNames(
+        files, filter = QFileDialog.getOpenFileNames(
             self, "Choose Files to Convert", init_dir, filter=FILTERS
         )
 
         if len(files) > 0:
             fileExtension = filter[filter.index("*"): -1]
             self.source_extension = fileExtension
-            self.dest_extension = self.formatList
             src_dir_name = str(Path(files[0]).parent)
             self.dirEdit.setText(src_dir_name)
             for file in files:
                 self.files.append(Path(file))
                 self.srcFileList.addItem(file)
             self.files_count = len(self.files)
+            self.updateStateWhenFilesLoaded()
+
+    def updateStateWhenFilesLoaded(self):
+        self.convertButton.setEnabled(True)
+        self.convertButton.setFocus(True)
+
+    def convert_files(self):
+        self.run_convert_thread()
+
+    def run_convert_thread(self):
+        dest_extension = str(self.formatList.currentText())
+        self.thread = QThread()
+        self.converter = Converter(
+            files=tuple(self.files),
+            dest_extension=dest_extension,
+        )
+        self.converter.moveToThread(self.thread)
+        # convert files with ffmpeg
+        self.thread.started.connect(self.converter.convert_files)
+        # update state
+        self.converter.converted_file.connect(self.updateStateWhenFilesConverted)
+        self.converter.progressed.connect(self.update_progress_bar)
+        self.converter.finished.connect(self.updateStateWhenNoFiles)
+        # clean up
+        self.converter.finished.connect(self.thread.quit)
+        self.converter.finished.connect(self.converter.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        # run the thread
+        self.thread.start()
+
+    def updateStateWhenFilesConverted(self, new_file):
+        self.files.popleft()
+        self.srcFileList.TakeItem(0)
+        self.dstFileList.addItem(str(new_file))
+
+    def update_progress_bar(self, file_number):
+        progress_percent = int(file_number / self.files_count * 100)
+        self.progressBar.setValue(progress_percent)
